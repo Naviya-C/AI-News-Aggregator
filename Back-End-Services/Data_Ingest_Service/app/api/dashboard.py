76dd2dff57c40b79a872f6ed265806a641aacb3f
Naveen <naviyac491@gmail.com>
2026-05-07 20:05:52 +0530

    implement dashboard route

--- Back-End-Services/Data_Ingest_Service/app/api/dashboard.py
+++ Back-End-Services/Data_Ingest_Service/app/api/dashboard.py
@@ -9,6 +9,22 @@ from app.models.news import NewsArticle, Recommendation
 
 router = APIRouter(prefix = "user")
 
+security = HTTPBearer()
 
+def get_user_id(credentials:str = Depends(security)):
+    token = credentials.credentials
+    payload = validation(token)
+    user_id:str = payload.get("sub")
+    
+    return int(user_id)
+        
+@router.get("/dashboard")
+def dashboard(db: Session = Depends(get_db), user_id:int = Depends(get_user_id)):
+    results = (
+        db.query(NewsArticle).join(Recommendation, NewsArticle.id == Recommendation.news_id)
+        .filter(Recommendation.user_id == user_id).all()
+    )
+    
+    return results
     
     
